e245c98988030cb3ba1c084c3a55aaa9fbe50051
nununuam <nunusnuam@gmail.com>
2022-02-28 11:28:05 -0800

    Done with updatePost & destroyPost Function

--- server/controllers/profilePage.js
+++ server/controllers/profilePage.js
@@ -43,8 +43,70 @@ const userPosts = (req, res) =>{
   })
 }
 
+const postComments = (req, res) =>{
+  db.Posts.findById(req.params.id, (err, foundPosts) => {
+    if(err) {
+      return res.status(400).json({
+        message: "cannot find post",
+        error: err
+      })
+    }
+    foundPosts.Comments.find().exec((err, allComments) => {
+      if (err){
+        return res.status(400),json({
+          message: "cannot find all the user posts",
+          error: err
+        })
+      }
+      return res.status(200).json({
+        message: "found the comments on the post",
+        data: allComments
+      })
+
+    })
+
+  })
+}
+
+const updatePost = (req, res) => {
+  db.Posts.findByIdAndUpdate (
+    req.params.id,
+    req.body,
+    {new:true}, (err, updatedPost) => {
+      if (err) {
+        retrun res.status(400).json({
+          message: "cannot update post",
+          error: err
+        })
+      }
+      return res.status(202).json({
+        message: "post update is a success",
+        data: updatedPost
+      })
+    }
+
+  )
+}
+
+const destroyPost = (req, res) => {
+    db.Posts.findByIdAndDelete(req.params.id, (err, deletedPost) => {
+        if (err){
+            return res.status(400).json({
+                message: "cannot delete post",
+                error: err,
+            })
+        }
+        return res.status(200).json({
+            message: "Post deletion is a Success!",
+            data: deletedPost
+        })
+    })
+}
+
 module.export{
 profileInfos,
 userPosts,
-
+postComments,
+updatePost,
+destroyPost,
 }
