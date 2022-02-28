eb1e5bf4a86a02a43aa6c308005cbd63774f3345
nununuam <nunusnuam@gmail.com>
2022-02-28 11:28:05 -0800

    fucntion for finding all the posts under that user is completed

--- server/controllers/profilePage.js
+++ server/controllers/profilePage.js
@@ -1,15 +1,15 @@
 const db = require("../models")
 
-const profileShow = (req,res) => {
+const profileInfos = (req,res) => {
   try{
-    const foundUser = await db.Profile.findById(req.userId)
+    const foundUser = await db.Profile.findById(req.params.id)
     return res.status(200).json({
       message:"found user profile",
       data: foundUser
     })
   }
   catch(err) {
-    sondole.log(err)
+    console.log(err)
     return res
       .status(500)
       .json({status:500,
@@ -18,6 +18,33 @@ const profileShow = (req,res) => {
   }
 }
 
+const userPosts = (req, res) =>{
+  db.Profile.findById(req.params.id, (err, foundProfile) => {
+    if(err) {
+      return res.status(400).json({
+        message: "cannot find profile by id",
+        error: err
+      })
+    }
+    foundProfile.Posts.find().exec((err, allPosts) => {
+      if (err){
+        return res.status(400),json({
+          message: "cannot find all the user posts",
+          error: err
+        })
+      }
+      return res.status(200).json({
+        message: "found all the user posts",
+        data: allPosts
+      })
+
+    })
+
+  })
+}
+
 module.export{
+profileInfos,
+userPosts,
 
 }
