a749e6e1f8d56e8a9c729ab727f3fddf80e6148e
nununuam <nunusnuam@gmail.com>
2022-02-28 11:28:05 -0800

    done with the axios file

--- client/src/api/axios.config.js
+++ client/src/api/axios.config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-cosnt backendAPI = "";
+cosnt backendAPI = "http://localhost:4000/api";
+//will be changed to this for Heroku https://quack-application.herokuapp.com/api
 
 const clientAPIs = axios.create({
     baseURL: `${backendAPI}`,
