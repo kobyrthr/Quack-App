a393c900fd27400c1e59db4e1ca12b7bb52cff0b
sabinaTT <tairsbn@gmail.com>
2022-03-19 14:47:12 -0700

    fix profile css

--- client/src/Pages/EditProfile/index.js
+++ client/src/Pages/EditProfile/index.js
@@ -3,79 +3,90 @@ import './index.css';
 const EditProfilePage = () => {
     return (
         <div class="container">
-
-            <div class="row">
-
-                <div class="columns six">
-                    
-                        <h5>Basic Info</h5>
+            <h5>Basic Info</h5>
+                <div class="row">
+                    <div class="columns six">
                         <label>First Name</label>
                         <input 
                                type="text"
                                class="firstname" 
                                placeholder="First Name">
                         </input>
-                        <label>Title</label>
-                        <input     
-                            type="text"    
-                            class="user-title"
-                            placeholder="Title">
-                        </input>
-            </div>
-            <div class="columns six">
+                    </div>
+     
+                    <div class="columns six">
                         <label>Last Name</label>
                         <input 
                                type="text"
                                class="lastname" 
                                placeholder="Last Name">
                         </input>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="columns six">
+                        <label>Title</label>
+                        <input     
+                            type="text"    
+                            class="user-title"
+                            placeholder="Title">
+                        </input>
+                    </div>
+                    <div class="columns six">
                         <label>Place Of Employment</label>
                         <input     
                             type="text"    
                             class="employment"
                             placeholder="Place of Employment">
                         </input>
+                    </div>
                 </div>
-
-            </div>
-            <div class="row">
-                <div class="columns twelve">
-                    <label>Bio</label>
+                <div class="row">
+                    <div class="columns twelve">
+                        <label>Bio</label>
+                        <input 
+                            type="text"
+                            class="bio" 
+                            placeholder="Briefly about me...">
+                        </input>
+                    </div>
+                <div class="row">
+                    <div class="columns six">
+                    <br />
+                        <h5>Socials</h5>
+                        <label>LinkedIn</label>
+                        <input 
+                            type="text"
+                            class="linkedin" 
+                            placeholder="LinkedIn URL">
+                        </input>
+                        <div class="columns six">
+                        <label>GitHub</label>
                         <input 
                                type="text"
-                               class="bio" 
-                               placeholder="Briefly about me...">
+                               class="github" 
+                               placeholder="GitHub URL">
                         </input>
-                </div>
-                <div clas="row">
-                <div class="columns six">
-                    
-                    <h5>Socials</h5>
-                    <label>LinkedIn</label>
-                    <input 
-                           type="text"
-                           class="linkedin" 
-                           placeholder="LinkedIn URL">
-                    </input>
-                    <label>Portfolio Site</label>
-                    <input     
-                        type="text"    
-                        class="portfolio-site"
-                        placeholder="Portfolio URL">
-                    </input>
-                    <label>Other</label>
-                    <input     
-                        type="text"    
-                        class="other"
-                        placeholder="Other">
-                    </input>
+                        </div>
+                        <label>Portfolio Site</label>
+                        <input     
+                            type="text"    
+                            class="portfolio-site"
+                            placeholder="Portfolio URL">
+                        </input>
+                        <label>Other</label>
+                        <input     
+                            type="text"    
+                            class="other"
+                            placeholder="Other">
+                        </input>
                     {/* <div class="row">
                         <button class="button-primary">Sign Up</button>
                         <button class="button-secondary">Log In</button>
                     </div> */}
         </div>
 
-
+        <div class="row"></div>
         <div class="columns six">
                         <label>GitHub</label>
                         <input 
@@ -100,7 +111,7 @@ const EditProfilePage = () => {
                     <h5>Reset Password</h5>
                     <label>Current Password</label>
                     <input
-                            type="text"    
+                            type="password"    
                             class="current-password"
                             placeholder="Enter Current Password">
                     </input>
@@ -111,7 +122,7 @@ const EditProfilePage = () => {
                 <div class="columns six">
                     <label>New Password</label>
                     <input
-                            type="text"    
+                            type="password"    
                             class="new-password"
                             placeholder="Enter New Password">
                     </input>
@@ -119,51 +130,18 @@ const EditProfilePage = () => {
                 <div class="columns six">
                     <label>Confirm New Password</label>
                     <input
-                            type="text"    
+                            type="password"    
                             class="confirm-new-password"
                             placeholder="Confirm New Password">
                     </input>
                 </div>
                 </div>
+                <br />
                 <div class="row">
                           <button class="button-secondary">Save Changes</button>
                           </div>
 
         </div>
-
-            
-            
-            
-            
-            
-        //     {/* <div class="row">
-        //                 <div class="columns six">
-                            
-        //                 <label>Last Name</label>
-        //                     <input 
-        //                         type="text"
-        //                         class="lastname" 
-        //                         placeholder="Last Name">
-        //                     </input>
-        //                     <label>Place Of Employment</label>
-        //                     <input    
-        //                         type="text"     
-        //                         class="placeOfEmployemnt"                       
-        //                         placeholder="Place of Employment"
-        //                     ></input>
-
-
-        //                 </div>
-                    
-        //                 <br />
-        //                     <button class="button-primary signupbtn">Sign Up</button>
-        //                     <button class="button-secondary loginbtn">Log In</button>
-                    
-        //         </div>
-
-        //     </div>
-
-        // </div> */}
     )
 }
 
