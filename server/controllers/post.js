b06fad3ec4da0484921d718d1544df7206d01ed5
kobyrthr-dev <kobyrthr@gmail.com>
2022-03-19 11:26:55 -0400

    getting db.posts underfined from my post controller

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,21 +1,11 @@
-const Posts = require('../models')
+const db = require('../models')
 
-async function index(req,res){
+ function index(req,res){
     res.send('Post Page here!'),
-    await Posts.create({
-        content: "Hi, I'm a post!"
-    }),
-    function (err) {
-        if (err) return handleError(err)
-        else console.log(Posts)
-    }
-}
-function logPost(req,res){
-    // console.log(post)
-
+    console.log(db.Posts)
 }
 
 module.exports = {
     index,
-    logPost
+    
 }
