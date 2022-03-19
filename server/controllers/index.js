59e25224e9fca13b887fb912edbeae1993b46a1a
kobyrthr-dev <kobyrthr@gmail.com>
2022-03-19 11:03:19 -0400

    removed profile page files. still connecting to db. post page renders test message

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,15 +1,8 @@
 
 const {signUp, logIn, userBoard} = require("./auth");
-const {profileInfo, userPosts, postComments, updatePost, destroyPost,} = require("./profilePage");
-
 
 module.exports = {
     signUp,
 	logIn,
-    userBoard,
-    profileInfo, 
-    userPosts, 
-    postComments, 
-    updatePost, 
-    destroyPost
+    userBoard
 }
