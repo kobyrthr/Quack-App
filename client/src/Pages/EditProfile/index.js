16ab509eb44234cb49e763b66175b87ef982eee9
sabinaTT <tairsbn@gmail.com>
2022-03-19 15:29:55 -0700

    complete edit profile css (minus nav bar)

--- client/src/Pages/EditProfile/index.js
+++ client/src/Pages/EditProfile/index.js
@@ -97,55 +97,56 @@ const EditProfilePage = () => {
                     </div> */}
 
 
-        <div class="row">
-        <div class="columns six">
+                <div class="row">
+                     <div class="columns six">
                         <label>Twitter</label>
                         <input     
                             type="text"    
                             class="twitter"
                             placeholder="Twitter URL">
                         </input>
+                    </div>
                 </div>
-                </div>
-
-
 
             <hr></hr>
-            <div class="row">
-                <div class="columns six">
-                    <h5>Reset Password</h5>
-                    <label>Current Password</label>
-                    <input
+
+                <div class="row">
+                    <div class="columns six">
+                        <h5>Reset Password</h5>
+                        <label>Current Password</label>
+                        <input
                             type="password"    
                             class="current-password"
                             placeholder="Enter Current Password">
-                    </input>
+                        </input>
+                    </div>
                 </div>
-
-            </div>
-            <div class="row">
-                <div class="columns six">
-                    <label>New Password</label>
-                    <input
+                <div class="row">
+                    <div class="columns six">
+                        <label>New Password</label>
+                        <input
                             type="password"    
                             class="new-password"
                             placeholder="Enter New Password">
-                    </input>
-                </div>
-                <div class="columns six">
-                    <label>Confirm New Password</label>
-                    <input
+                        </input>
+                    </div>
+                    <div class="columns six">
+                        <label>Confirm New Password</label>
+                        <input
                             type="password"    
                             class="confirm-new-password"
                             placeholder="Confirm New Password">
-                    </input>
+                        </input>
+                    </div>
                 </div>
-                </div>
-                <br />
-                <div class="row">
-                          <button class="button-secondary">Save Changes</button>
-                          </div>
 
+                <br />
+
+                <div class="row">
+                    <button class="button-secondary">Save Changes</button>
+                </div>
+            <br />
+            <br />
         </div>
     )
 }
