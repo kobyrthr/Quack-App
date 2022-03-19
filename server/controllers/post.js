316537b89af16be2540fe992d8be566708d986a8
kobyrthr-dev <kobyrthr@gmail.com>
2022-03-19 12:43:44 -0400

    databse connection working

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,11 +1,29 @@
-const db = require('../models')
+const Post = require('../models/Posts')
 
  function index(req,res){
     res.send('Post Page here!'),
-    console.log(db.Posts)
+    Post.find().exec((err,allPosts)=>{
+        if (err){
+            return res.status(400), json({
+                message:"can't find all the posts",
+                error:err
+            })
+        }
+        else {console.log(allPosts)}
+    }
+
+    )
 }
 
 module.exports = {
     index,
     
 }
+
+// .Posts.find().exec((err, allPosts) => {
+//     if (err){
+//       return res.status(400),json({
+//         message: "cannot find all the user posts",
+//         error: err
+//       })
+//     }
--- server/server.js
+++ server/server.js
@@ -15,6 +15,21 @@ const routes = require('./routes');
 /* === Configuration === */
 const config = require('@Quack-App/config');
 
+/* === Setup Mongoose=== */
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const dbUrl = process.env.MONGO_URL;
+
+mongoose   
+    .connect(dbUrl)
+    .then(() => {
+        console.log(`MongoDB connected at ${db.host}: ${db.port}.`)
+    })
+    .catch((err) => { 
+        console.log(`MongoDB failed to connect. Error: ${err}`)
+    });
+
+
 /* === Middleware === */
 app.use(express.static(path.join('build')));
 app.use(express.urlencoded({extended: true}));
