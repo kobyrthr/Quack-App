7c0e0417bcb36fd89a740f780ffb598bf7c4ac81
sabinaTT <tairsbn@gmail.com>
2022-03-19 11:23:39 -0700

    finish login page css

--- client/src/Components/Login/index.jsx
+++ client/src/Components/Login/index.jsx
@@ -17,7 +17,7 @@ export default function LoginPage() {
                         <h6>Username</h6>
                         <input class="username" placeholder="Email"></input>
                         <h6>Password</h6>
-                        <input class="password" placeholder="Password"></input>
+                        <input class="password" type="password" placeholder="Password"></input>
                         <br />
                             <button class="button-primary signupbtn">Sign Up</button>
                             <button class="button-secondary loginbtn">Log In</button>
